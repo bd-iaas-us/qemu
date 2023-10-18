56215aa2910f3987083a73e8ed7da9b1176a3a02
Hao Xiang <hao.xiang@bytedance.com>
2023-10-18 17:18:12 +0000

    migration/multifd: Add new migration option for multifd DSA offloading.
    Intel DSA offloading is an optional feature that turns on if
    proper hardware and software stack is available. To turn on
    DSA offloading in multifd live migration:
    
    multifd-dsa-accel="[dsa_dev_path1] ] [dsa_dev_path2] ... [dsa_dev_pathX]"
    
    This feature is turned off by default.
    
    Signed-off-by: Hao Xiang <hao.xiang@bytedance.com>

--- migration/options.h
+++ migration/options.h
@@ -91,6 +91,7 @@ const char *migrate_tls_authz(void);
 const char *migrate_tls_creds(void);
 const char *migrate_tls_hostname(void);
 uint64_t migrate_xbzrle_cache_size(void);
+const char *migrate_multifd_dsa_accel(void);
 
 /* parameters setters */
 
